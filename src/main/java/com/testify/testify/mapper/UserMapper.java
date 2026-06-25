1e99d6f730dcc24469bd27bf650b75ed6190de3c
chris <christopher.mutuma@dlight.com>
2026-06-25 13:38:52 +0300

    resolving test suite 404-new

--- src/main/java/com/testify/testify/mapper/UserMapper.java
+++ src/main/java/com/testify/testify/mapper/UserMapper.java
@@ -3,12 +3,9 @@ package com.testify.testify.mapper;
 import com.testify.testify.dto.UserDto;
 import com.testify.testify.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     UserDto toUserDto(User user);
 }
