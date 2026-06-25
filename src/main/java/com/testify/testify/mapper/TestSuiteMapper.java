fe82528413946dcf2df29f6dfac0981f78ef88e9
chris <christopher.mutuma@dlight.com>
2026-06-25 13:24:06 +0300

    resolving test suite 404

--- src/main/java/com/testify/testify/mapper/TestSuiteMapper.java
+++ src/main/java/com/testify/testify/mapper/TestSuiteMapper.java
@@ -6,12 +6,9 @@ import com.testify.testify.entity.TestSuite;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", uses = UserMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TestSuiteMapper {
-
-    TestSuiteMapper INSTANCE = Mappers.getMapper(TestSuiteMapper.class);
 
     @Mapping(source = "createdBy", target = "createdBy")
     TestSuiteResponse toTestSuiteResponse(TestSuite testSuite);
