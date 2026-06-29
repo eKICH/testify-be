d00f904db35964fe3d8b05a20d29eb50bb8c0b15
chris <christopher.mutuma@dlight.com>
2026-06-29 12:48:43 +0300

    Fix: Use System.getenv() for database config

--- src/main/java/com/testify/testify/config/RenderDataSourceConfig.java
+++ src/main/java/com/testify/testify/config/RenderDataSourceConfig.java
@@ -1,6 +1,5 @@
 package com.testify.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,14 +13,22 @@ public class RenderDataSourceConfig {
 
     @Bean
     public DataSource dataSource() {
-        // Build DataSource from individual components
+        String host = System.getenv("DB_HOST");
+        String port = System.getenv("DB_PORT");
+        String database = System.getenv("DB_DATABASE");
+        String username = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        // Build the JDBC URL manually
+        String url = "jdbc:postgresql://" + host + ":" + port + "/" + database + "?ssl=true&sslmode=require";
+
+        System.out.println("Connecting to: " + url.replaceAll(password, "****"));
+
         return DataSourceBuilder.create()
                 .driverClassName("org.postgresql.Driver")
-                .url("jdbc:postgresql://" + System.getenv("DB_HOST") + ":" +
-                        System.getenv("DB_PORT") + "/" + System.getenv("DB_DATABASE") +
-                        "?ssl=true&sslmode=require")
-                .username(System.getenv("DB_USER"))
-                .password(System.getenv("DB_PASSWORD"))
+                .url(url)
+                .username(username)
+                .password(password)
                 .build();
     }
 }
