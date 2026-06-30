a744c77e8c6de9a4d94e87f9d809f499f395d26b
chris <christopher.mutuma@dlight.com>
2026-06-30 21:29:18 +0300

    update: added component scan

--- src/main/java/com/testify/testify/TestifyApplication.java
+++ src/main/java/com/testify/testify/TestifyApplication.java
@@ -2,8 +2,10 @@ package com.testify.testify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.testify.testify")
 public class TestifyApplication {
 
 	public static void main(String[] args) {
