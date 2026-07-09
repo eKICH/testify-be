6961f9532944918d33f1e0ec3befb592cc48e4b3
chris <christopher.mutuma@dlight.com>
2026-07-09 10:17:08 +0300

    fix:testplan service end of file error

--- src/main/java/com/testify/testify/service/TestPlanService.java
+++ src/main/java/com/testify/testify/service/TestPlanService.java
@@ -1,6 +1,5 @@
 package com.testify.testify.service;
 
-import com.testify.testify.dto.TestPlanRequest;
 import com.testify.testify.dto.TestPlanCreateRequest;
 import com.testify.testify.dto.TestPlanResponse;
 import org.springframework.data.domain.Page;
@@ -9,12 +8,18 @@ import org.springframework.data.domain.Pageable;
 import java.util.UUID;
 
 public interface TestPlanService {
-    TestPlanResponse createTestPlan(TestPlanRequest request, UUID userId);
+
     TestPlanResponse createTestPlan(TestPlanCreateRequest request, UUID userId);
+
     TestPlanResponse getTestPlan(Long id);
+
     Page<TestPlanResponse> getAllTestPlans(Pageable pageable);
-    TestPlanResponse updateTestPlan(Long id, TestPlanRequest request, UUID userId);
+
     TestPlanResponse updateTestPlan(Long id, TestPlanCreateRequest request, UUID userId);
+
     void deleteTestPlan(Long id);
+
     TestPlanResponse addTestCaseToTestPlan(Long testPlanId, UUID testCaseId, UUID userId);
+
     TestPlanResponse removeTestCaseFromTestPlan(Long testPlanId, UUID testCaseId, UUID userId);
+}
